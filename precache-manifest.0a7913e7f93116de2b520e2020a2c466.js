ed89c8f4ea1a26fe4472ce6558dc269b10eab99a
Alejo Castaño <alejocas98@gmail.com>
2019-03-30 15:56:42 -0500

    Deploy Alejo's page

---
+++ precache-manifest.0a7913e7f93116de2b520e2020a2c466.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "ad27c55f910bd3ede47c",
+    "url": "/js/chunk-vendors.44b8419f.js"
+  },
+  {
+    "revision": "d062d4f8d17cb26c8917",
+    "url": "/js/app.2746869f.js"
+  },
+  {
+    "revision": "d89be0ebfcb2760c06c9",
+    "url": "/js/about.1383eeaa.js"
+  },
+  {
+    "revision": "bb7ec1a284041bfc8941d6fccac53fe1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cac4ae4476b3ed6801d6b4ebf9af17e1",
+    "url": "/img/glass-compass-maps.cac4ae44.jpg"
+  },
+  {
+    "revision": "d062d4f8d17cb26c8917",
+    "url": "/css/app.4de95c83.css"
+  }
+];
